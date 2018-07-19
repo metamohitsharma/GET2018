67c6d43be1c8d2bab94cfa059d861a4bf778239a
metamohitsharma <mohit.sharma@metacube.com>
2018-07-19 22:52:37 +0530

    Updated Stack using Array

--- PF/PF-Assignment-1/Stack using Array/Implementation.java
+++ PF/PF-Assignment-1/Stack using Array/Implementation.java
@@ -1,70 +1,62 @@
+package StackArray;
+
 import java.util.Scanner;
 
 /**
  * It contains the main method which implements the StackArray class
- * @author user37
+ * 
+ * @author Mohit Sharma
  *
  */
 public class Implementation {
-	
 	/**
 	 * It is the main method of the Program, it implements Stack using Array
+	 * 
 	 * @param args command line argument
 	 */
 	public static void main(String[] args) {
-	        StackArray stackArray = new StackArray();
-	        while(true) {
-	        	System.out.println("Choose any Operation:\n1. Push Element\n2. Pop Element\n"
-				+ "3. Check whether Stack is empty or not\n4. Topmost Element\n5. Exit");
-	        	Scanner inputChoice = new Scanner(System.in);
-        		String operation = inputChoice.nextLine();
-        		switch (operation) {
-        		case "1":
-        			try {
-        				System.out.println("Enter the Element to be addded");
-        				String enteredElement = inputChoice.nextLine();
-        				boolean status = stackArray.push(enteredElement);
-        				if (status) {
-        					System.out.println("Element Entered");
-        				}
-        			} catch (StackException e) {
-        				System.out.println(e.getMessage());
-        			}
-        			break;
-			            
-        		case "2":
-        			try {
-        				String deletedElement = stackArray.pop();
-        				System.out.println("Deleted Element:- " + deletedElement);
-        			} catch (StackException e) {
-        				System.out.println(e.getMessage());
-        			}
-        			break;
-			            
-        		case "3":
-        			boolean status = stackArray.isEmpty();
-        			if(status) {
-        				System.out.println("Stack is Empty");
-        			} else {
-        				System.out.println("Stack is not Empty");
-        			}
-        			break;  
-			            
-        		case "4":
-        			try {
-        				String topElement = stackArray.top();
-        				System.out.println("Topmost Element:- " + topElement);
-        			} catch(StackException e) {
-        				System.out.println(e.getMessage());
-        			}
-        			break;
-			            
-        		case "5": 
-        			System.exit(1);
-			        
-        		default:
-        			System.out.println("Wrong Input");
-        		}
-	        }
+		StackArray stackArray = new StackArray();
+		boolean status;
+		while (true) {
+			System.out.println("Choose any Operation:\n1. Push Element\n2. Pop Element\n"
+					+ "3. Check whether Stack is empty or not\n4. Topmost Element\n5. Exit");
+			String operation = new Scanner(System.in).nextLine();
+			switch (operation) {
+			case "1":
+
+				System.out.println("Enter the Element to be addded");
+				String enteredElement = new Scanner(System.in).nextLine();
+				status = stackArray.push(enteredElement);
+				if (status) {
+					System.out.println("Element Entered");
+				}
+				break;
+
+			case "2":
+				String deletedElement = stackArray.pop();
+				System.out.println("Deleted Element:- " + deletedElement);
+				break;
+
+			case "3":
+				status = stackArray.isEmpty();
+				if (status) {
+					System.out.println("Stack is Empty");
+				} else {
+					System.out.println("Stack is not Empty");
+				}
+				break;
+
+			case "4":
+				String topElement = stackArray.top();
+				System.out.println("Topmost Element:- " + topElement);
+				break;
+
+			case "5":
+				System.exit(1);
+
+			default:
+				System.out.println("Wrong Input");
+			}
+		}
 	}
 }
--- PF/PF-Assignment-1/Stack using Array/StackArray.java
+++ PF/PF-Assignment-1/Stack using Array/StackArray.java
@@ -1,67 +1,84 @@
+package StackArray;
 
 /**
  * This class describes the implementation of Stack using array.
- * @author user37
+ * 
+ * @author Mohit Sharma
  *
  */
-class StackArray {
-	protected String	stack[] = new String[5];
-	private final int 	STACK_SIZE = 4;
-	private int 		topElement = -1;
-	
+public class StackArray {
+	protected String stackArray[] = new String[5];
+	private final int STACK_SIZE = 4;
+	private int topElement = -1;
+
 	/**
 	 * Push the given element to Stack
+	 * 
 	 * @param new_element to be pushed to Stack
 	 * @return true if new_element is pushed to Stack
 	 * @throws StackException, if the Stack Overflows
 	 */
-	public boolean push(String new_element) throws StackException {
-	        if (topElement == STACK_SIZE) {
-	            throw new StackException("Stack Overflow");
-	        }
-	        topElement++;
-	        stack[topElement]=new_element;
-	        return true;
-        }
-	
+	public boolean push(String newElement) throws ArrayIndexOutOfBoundsException {
+		try {
+			if (topElement == STACK_SIZE) {
+				throw new ArrayIndexOutOfBoundsException("Stack Overflow");
+			}
+			topElement++;
+			stackArray[topElement] = newElement;
+			return true;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+			return false;
+		}
+	}
+
 	/**
 	 * Pop the Top Element in Stack
-	 * @return Popped Element from Stack 
+	 * 
+	 * @return Popped Element from Stack
 	 * @throws StackException, if the Stack Underflows
 	 */
-	public String pop() throws StackException {
-		String	deletedItem;
-	        if (topElement == -1) {
-	            throw new StackException("Stack Underflow");
-	        }
-	        deletedItem = stack[topElement];
-	        topElement--;
-	        return deletedItem;
-        }
-	
+	public String pop() throws ArrayIndexOutOfBoundsException {
+		try {
+			String deletedItem;
+			if (topElement == -1) {
+				throw new ArrayIndexOutOfBoundsException("Stack Underflow");
+			}
+			deletedItem = stackArray[topElement];
+			topElement--;
+			return deletedItem;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+			return "false";
+		}
+	}
+
 	/**
 	 * It checks whether the Stack is Empty or not
+	 * 
 	 * @return true if the Stack is Empty
 	 * @throws StackException
 	 */
 	public boolean isEmpty() {
-	        if (topElement == -1) {
-	            return true;
-	        } else {
-	            return false;
-	        }
+		return (topElement == -1 ? true : false);
 	}
-	
+
 	/**
 	 * It returns the Top Element of Stack
+	 * 
 	 * @return Top Element Stored at Stack
 	 * @throws StackException, if Stack Underflows
 	 */
-	public String top() throws StackException {
-	        if (topElement == -1) {
-	            throw new StackException("Stack Underflow");
-	        } else {
-	            return stack[topElement];
-	        } 
+	public String top() throws ArrayIndexOutOfBoundsException {
+		try {
+			if (topElement == -1) {
+				throw new ArrayIndexOutOfBoundsException("Stack Underflow");
+			} else {
+				return stackArray[topElement];
+			}
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+			return "false";
+		}
 	}
 }
