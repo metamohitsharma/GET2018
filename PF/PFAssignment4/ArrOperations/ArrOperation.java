3ee0e937c8458327da569c39c6dedc01410b0551
metamohitsharma <mohit.sharma@metacube.com>
2018-07-29 15:34:43 +0530

    Updated With Char Array

--- PF/PFAssignment4/ArrOperations/ArrOperation.java
+++ PF/PFAssignment4/ArrOperations/ArrOperation.java
@@ -11,21 +11,30 @@ import java.util.ArrayList;
 public class ArrOperation {
 
 	/**
-	 * Computes largest mirror size in Array Mirror section in an array is a
-	 * group of contiguous elements such that somewhere in the array, the same
-	 * group appears in reverse order
+	 * Computes largest mirror size in Array Mirror section in an array is a group
+	 * of contiguous elements such that somewhere in the array, the same group
+	 * appears in reverse order
 	 * 
 	 * @param mirrorArray
 	 * @return largestMirror
+	 * @throws ArrOperationException
 	 */
-	int largestMirror(int mirrorArray[]) {
-		if (mirrorArray.length == 0) {
-			throw new AssertionError("Input Array is Empty");
+	int largestMirror(char mirrorArray[]) throws ArrOperationException {
+		if (mirrorArray == null) {
+			throw new NullPointerException("Input Array is Null in largestMirror");
+		} else if (mirrorArray.length == 0) {
+			throw new ArrOperationException("Input Array is Empty in largestMirror");
 		}
 		int startIndex = 0;
 		int endIndex = 0;
 		int mirrorSize = 0;
 		int maxMirrorSize = 0;
+
+		/*
+		 * Checking character equivalence, if found then increasing StartIndex and
+		 * decreasing EndIndex and if the countSize of new Mirror is greater than
+		 * maxSizeMirror then swap them
+		 */
 		for (int i = 0; i < mirrorArray.length; i++) {
 			startIndex = i;
 			mirrorSize = 0;
@@ -54,31 +63,39 @@ public class ArrOperation {
 	}
 
 	/**
-	 * Computes number of Clumps in an Array, Clump in an array is a series of 2
-	 * or more adjacent elements of the same value
+	 * Computes number of Clumps in an Array, Clump in an array is a series of 2 or
+	 * more adjacent elements of the same value
 	 * 
 	 * @param clumpsArray
 	 * @return numbersOfClumps
+	 * @throws ArrOperationException
 	 */
-	public int numbersOfClumps(int[] clumpsArray) {
-		if (clumpsArray.length == 0) {
-			throw new AssertionError("Array is Empty");
+	public int numbersOfClumps(char[] clumpsArray) throws ArrOperationException {
+		if (clumpsArray == null) {
+			throw new NullPointerException("Clump Array is Null");
+		} else if (clumpsArray.length == 0) {
+			throw new ArrOperationException("Clump Array is Empty");
+		} else if (clumpsArray.length == 1) {
+			return 0;
 		}
-		int numbersOfClumps = 0;
-		int clump = clumpsArray[0];
-		int clumpCount = 1;
-		for (int i = 1; i < clumpsArray.length; i++) {
-			if (clump == clumpsArray[i]) {
-				if (clumpCount == 1) {
-					numbersOfClumps++;
+		int noOfClumps = 0;
+		int startClump = 0; // This Variable is used to store Clump's starting Index
+
+		/*
+		 * Iterating over clumpsArray, Checking Element at i Index is equal to Element
+		 * at i+1 Index then if i is Equal to the Starting Index of Clump then
+		 * increasing noOfClumps
+		 */
+		for (int i = 0; i < clumpsArray.length - 1; i++) {
+			if (clumpsArray[i] == clumpsArray[i + 1]) {
+				if (startClump == i) {
+					noOfClumps++;
 				}
-				clumpCount++;
 			} else {
-				clump = clumpsArray[i];
-				clumpCount = 1;
+				startClump = i + 1;
 			}
 		}
-		return numbersOfClumps;
+		return noOfClumps;
 	}
 
 	/**
@@ -88,15 +105,29 @@ public class ArrOperation {
 	 * @param y
 	 * @param arrayToFixXy
 	 * @return
+	 * @throws ArrOperationException
 	 */
-	public int[] fixXy(int x, int y, int[] arrayToFixXy) {
-		if (arrayToFixXy.length == 0) {
-			throw new AssertionError("Array is Empty");
+	public char[] fixXy(char x, char y, char[] arrayToFixXY) throws ArrOperationException {
+		if (arrayToFixXY == null) {
+			throw new NullPointerException("ArrayToFixXY is Null");
+		} else if (x == '\0') {
+			throw new NullPointerException("Given X is Null");
+		} else if (y == '\0') {
+			throw new NullPointerException("Given Y is Null");
+		} else if (arrayToFixXY.length == 0) {
+			throw new ArrOperationException("ArrayToFixXY is Empty");
+		} else if (arrayToFixXY.length == 1) {
+			throw new ArrOperationException("ArrayToFixXY should atleast have two Elements");
 		}
-		int[] copyOfFixArray = arrayToFixXy;
+		char[] copyOfFixArray = arrayToFixXY;
 		if (x == copyOfFixArray[copyOfFixArray.length - 1]) {
-			throw new AssertionError(x + " is at Last Index");
+			throw new ArrOperationException(x + " is at Last Index");
 		}
+
+		/*
+		 * Taking Two ArrayList to save the Indexes of X and Y Element so that Y could
+		 * be placed right after X
+		 */
 		ArrayList<Integer> xIndex = new ArrayList<Integer>();
 		ArrayList<Integer> yIndex = new ArrayList<Integer>();
 		for (int i = 0; i < copyOfFixArray.length; i++) {
@@ -108,13 +139,18 @@ public class ArrOperation {
 			}
 		}
 		if (xIndex.size() != yIndex.size()) {
-			throw new AssertionError(x + " and " + y + " are not Equal");
+			throw new ArrOperationException("No of " + x + " and " + y + " are not Equal");
 		}
+
+		/*
+		 * Iterating over xIndex ArrayList, swapping the Next Element after X to Y
+		 * Element
+		 */
 		for (int i = 0; i < xIndex.size(); i++) {
 			if (i != xIndex.size() - 1 && xIndex.get(i) == xIndex.get(i + 1)) {
-				throw new AssertionError("Two " + x + " at adjacent Positions");
+				throw new ArrOperationException("Two " + x + " at adjacent Positions");
 			}
-			int swapY = copyOfFixArray[xIndex.get(i) + 1];
+			char swapY = copyOfFixArray[xIndex.get(i) + 1];
 			copyOfFixArray[xIndex.get(i) + 1] = copyOfFixArray[yIndex.get(i)];
 			copyOfFixArray[yIndex.get(i)] = swapY;
 		}
@@ -122,25 +158,37 @@ public class ArrOperation {
 	}
 
 	/**
-	 * Return the index if there is a place to split the input array so that the
-	 * sum of the numbers on one side is equal to the sum of the numbers on the
-	 * other side else return -1
+	 * Return the index if there is a place to split the input array so that the sum
+	 * of the numbers on one side is equal to the sum of the numbers on the other
+	 * side else return -1
 	 * 
 	 * @param enteredArray
 	 * @return
+	 * @throws ArrOperationException
 	 */
-	public int splitArray(int[] enteredArray) {
-		if (enteredArray.length == 0) {
-			throw new AssertionError("Array is Empty");
+	public int splitArray(int[] enteredArray) throws ArrOperationException {
+		if (enteredArray == null) {
+			throw new NullPointerException("Array is Null in SplitArray");
+		} else if (enteredArray.length == 0) {
+			throw new ArrOperationException("Array is Empty in SplitArray");
+		} else if (enteredArray.length == 1) {
+			throw new ArrOperationException("Array Length should be atleast 2 in SplitArray");
 		}
 		int leftHalf = 0;
 		int rightHalf = 0;
 		int splitIndex = -1;
 
+		/*
+		 * Adding all the Elements of Array from Right Side
+		 */
 		for (int i = 0; i < enteredArray.length; i++) {
 			rightHalf += enteredArray[i];
 		}
 
+		/*
+		 * Iterating over Array from Left and checking the Equality of Left and Right
+		 * side sum
+		 */
 		for (int i = 0; i < enteredArray.length; i++) {
 			if (leftHalf == rightHalf) {
 				splitIndex = i;
@@ -151,5 +199,18 @@ public class ArrOperation {
 			}
 		}
 		return splitIndex;
+	}
+
+	@SuppressWarnings("serial")
+	static class ArrOperationException extends Exception {
+		String messageException;
+
+		public ArrOperationException(String messageException) {
+			this.messageException = messageException;
+		}
+
+		public String getMessageException() {
+			return messageException;
+		}
 	}
 }
--- PF/PFAssignment4/ArrOperations/ArrOperationsTest.java
+++ PF/PFAssignment4/ArrOperations/ArrOperationsTest.java
@@ -1,9 +1,9 @@
 package GET2018.PF.PFAssignment4.ArrOperations;
 
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
+import GET2018.PF.PFAssignment4.ArrOperations.ArrOperation.ArrOperationException;
 
 /**
  * This class contains TestCases for different methods in ArrOperation Class
@@ -20,43 +20,162 @@ public class ArrOperationsTest {
 	}
 
 	@Test
-	public void largestMirrorTest() {
-		assertEquals(
-				3,
-				arrayOperations.largestMirror(new int[] { 1, 2, 3, 8, 9, 3, 2,
-						1 }));
-		assertEquals(2, arrayOperations.largestMirror(new int[] { 7, 1, 4, 9,
-				7, 4, 1 }));
-		assertEquals(3, arrayOperations.largestMirror(new int[] { 1, 2, 1, 4 }));
-		assertEquals(7, arrayOperations.largestMirror(new int[] { 1, 4, 5, 3,
-				5, 4, 1 }));
+	public void largestMirrorTest() throws ArrOperationException {
+		assertEquals(9, arrayOperations.largestMirror(new char[] { 'a', '@', 'b', ' ', '$', ' ', 'b', '@', 'a' }));
+		assertEquals(3, arrayOperations.largestMirror(new char[] { 'f', '3', 'f', 'x' }));
 	}
 
 	@Test
-	public void numberOfClumpsTest() {
-		assertEquals(2,
-				arrayOperations.numbersOfClumps(new int[] { 1, 2, 2, 3, 4, 4 }));
-		assertEquals(2,
-				arrayOperations.numbersOfClumps(new int[] { 1, 1, 2, 1, 1 }));
-		assertEquals(1,
-				arrayOperations.numbersOfClumps(new int[] { 1, 1, 1, 1, 1 }));
+	public void numberOfClumpsTest() throws ArrOperationException {
+		assertEquals(2, arrayOperations.numbersOfClumps(new char[] { 'a', 'a', 'b', '/', '/' }));
+		assertEquals(1, arrayOperations.numbersOfClumps(new char[] { ' ', ' ', ' ' }));
 	}
 
 	@Test
-	public void fixXYTest() {
-		assertArrayEquals(new int[] { 9, 4, 5, 4, 5, 9 },
-				arrayOperations.fixXy(4, 5, new int[] { 5, 4, 9, 4, 9, 5 }));
-		assertArrayEquals(new int[] { 1, 4, 5, 1 },
-				arrayOperations.fixXy(4, 5, new int[] { 1, 4, 1, 5 }));
-		assertArrayEquals(new int[] { 1, 4, 5, 1, 1, 4, 5 },
-				arrayOperations.fixXy(4, 5, new int[] { 1, 4, 1, 5, 5, 4, 1 }));
+	public void fixXYTest() throws ArrOperationException {
+		assertArrayEquals(new char[] { ' ', '@', 'b', 'a', '$', '@', 'b', 'a' },
+				arrayOperations.fixXy('b', 'a', new char[] { 'a', '@', 'b', ' ', '$', 'a', 'b', '@' }));
 	}
 
 	@Test
-	public void splitArrayTest() {
+	public void splitArrayTest() throws ArrOperationException {
 		assertEquals(3, arrayOperations.splitArray(new int[] { 1, 1, 1, 2, 1 }));
-		assertEquals(-1,
-				arrayOperations.splitArray(new int[] { 2, 1, 1, 2, 1 }));
+		assertEquals(-1, arrayOperations.splitArray(new int[] { 2, 1, 1, 2, 1 }));
 		assertEquals(1, arrayOperations.splitArray(new int[] { 10, 10 }));
+	}
+
+	@Test
+	public void largestMirrorWithNull() throws ArrOperationException {
+		try {
+			arrayOperations.largestMirror(null);
+		} catch (NullPointerException ex) {
+			assertEquals("Input Array is Null in largestMirror", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void largestMirrorLengthZero() throws ArrOperationException {
+		try {
+			arrayOperations.largestMirror(new char[] {});
+		} catch (ArrOperationException ex) {
+			assertEquals("Input Array is Empty in largestMirror", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void noOfClumpsWithNull() throws ArrOperationException {
+		try {
+			arrayOperations.numbersOfClumps(null);
+		} catch (NullPointerException ex) {
+			assertEquals("Clump Array is Null", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void noOfClumpsLengthZeroArray() throws ArrOperationException {
+		try {
+			arrayOperations.numbersOfClumps(new char[] {});
+		} catch (ArrOperationException ex) {
+			assertEquals("Clump Array is Empty", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithNull() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy(' ', ' ', null);
+		} catch (NullPointerException ex) {
+			assertEquals("ArrayToFixXY is Null", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithNullX() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('\0', ' ', new char[] { 'a', ' ' });
+		} catch (NullPointerException ex) {
+			assertEquals("Given X is Null", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithNullY() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy(' ', '\0', new char[] { 'a', ' ' });
+		} catch (NullPointerException ex) {
+			assertEquals("Given Y is Null", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithZeroLength() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('b', 'a', new char[] {});
+		} catch (ArrOperationException ex) {
+			assertEquals("ArrayToFixXY is Empty", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithUnitLength() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('b', 'a', new char[] { 'a' });
+		} catch (ArrOperationException ex) {
+			assertEquals("ArrayToFixXY should atleast have two Elements", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithXAtLastIndex() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('b', 'a', new char[] { 'a', 'b' });
+		} catch (ArrOperationException ex) {
+			assertEquals("b is at Last Index", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithXAndYUnEqual() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('b', 'a', new char[] { 'a', 'b', 'a' });
+		} catch (ArrOperationException ex) {
+			assertEquals("No of b and a are not Equal", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void arrayToFixWithTwoAdjacentX() throws ArrOperationException {
+		try {
+			arrayOperations.fixXy('b', 'a', new char[] { 'b', 'b', 'a', 'a' });
+		} catch (ArrOperationException ex) {
+			assertEquals("Two b at adjacent Positions", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void splitArrayWithNull() throws ArrOperationException {
+		try {
+			arrayOperations.splitArray(null);
+		} catch (NullPointerException ex) {
+			assertEquals("Array is Null in SplitArray", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void splitArrayWithZeroLength() throws ArrOperationException {
+		try {
+			arrayOperations.splitArray(new int[] {});
+		} catch (ArrOperationException ex) {
+			assertEquals("Array is Empty in SplitArray", ex.getMessageException());
+		}
+	}
+
+	@Test
+	public void splitArrayWithUnitLength() throws ArrOperationException {
+		try {
+			arrayOperations.splitArray(new int[] { 1 });
+		} catch (ArrOperationException ex) {
+			assertEquals("Array Length should be atleast 2 in SplitArray", ex.getMessageException());
+		}
 	}
 }
